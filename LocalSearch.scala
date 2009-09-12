2c292dac175d3e2fca7c1abdd173866884336d20
andres <andres@jengibre.com.ar>
2009-09-12 19:06:07 -0300

    refinement

--- LocalSearch.scala
+++ LocalSearch.scala
@@ -1,42 +1,48 @@
 import Params._
 
-object LocalSearch {
+object LocalSearchMain {
 	def main(args: Array[String]) {
 		val inst = Solomon.load(args(0))
 		val solver = new NearestNeighbour(inst)
 
-		def sumd(l: List[Customer]): Double = {
-			l.zip(l.tail).foldLeft(0.0)((x, y) => x + inst.distancia(y._1, y._2))
-		}
-
 		val mejor = solver.solve
-		val mejorLargo = mejor.foldLeft(0.0)(_ + sumd(_))
-		
-		println("NN = " + mejor.map(_.map(_.num)))
-		println("NN length = " + mejorLargo)
-		
-		println("busqueda local!")
-		dosOpt(mejor)
-	}
 	
-	def dosOpt(sol: List[List[Customer]]): List[List[Customer]] = {
-		// pruebo hacer relocate del primer camion
-		val rnd = new scala.util.Random()
+		val ls = new LocalSearch(inst, mejor)
+		ls.relocate()
+	}
+}
 
-		val customer = sol(0)(1+rnd.nextInt(sol(0).length-1))
-		println("relocate de " + customer.num)
+class LocalSearch(inst: Instance, sol: List[List[Customer]]) {
+	val rnd = new scala.util.Random()
 
-		println("camion original = " + sol(0).map(_.num))
-		val camion = sol(0) - customer
-		println("camion = " + camion.map(_.num))
-		println("")
+	def sumd(l: List[Customer]): Double = {
+		l.zip(l.tail).foldLeft(0.0)((x, y) => x + inst.distancia(y._1, y._2))
+	}
+
+	def relocate() = {
+		def relocateCamion(camion: List[Customer]) = {
+			def relocateCustomer(camion: List[Customer], customer: Customer) = {
+				println("relocate de " + customer.num)
+
+				println("camion original = " + camion.map(_.num))
+				println("largo = " + sumd(camion))
+
+				val camionIt = camion - customer
+				println("camion = " + camionIt.map(_.num))
 		
-		for(i <- 1 to camion.length) {
-			val l = camion.take(i) ::: List(customer) ::: camion.drop(i)
-			println(l.map(_.num))
-			println(
+				for(i <- 1 to camionIt.length) {
+					val l = camionIt.take(i) ::: List(customer) ::: camionIt.drop(i)
+					if (l != camion && inst.camionFactible(l)) {
+						println("new factible: " + l.map(_.num))
+						println("new largo = " + sumd(l))
+					}
+				}
+				println("")
+			}
+
+			camion.tail.foreach(relocateCustomer(camion, _))
 		}
 
-		Nil
-	}
+		sol.foreach(relocateCamion _)
+	}	
 }
